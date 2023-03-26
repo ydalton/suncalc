6b6fd4021163ff31b99bd8ecc083db267c430ecd
ydalton <r0930164@student.thomasmore.be>
2023-03-26 22:59:33 +0200

    Added more functions

--- Program.cs
+++ Program.cs
@@ -15,29 +15,42 @@ namespace BesselCli {
         // the obliquity/tilt of the Earth in degrees
         const double obliquity = 23.44;
         const double yearLength = 365.242;
+        // get declension of the sun
+        public static double GetDeclension(int day) {
+            return obliquity * Math.Sin(2 * Math.PI * (day/yearLength) - 1.4);
+        }
+
         // get time of sunset
-        public static double GetHours() {
-            return 0;
+        public static double GetHours(double lat, double decl) {
+            double hours = Math.Acos(-Math.Tan(lat) * Math.Tan(decl));
+            return hours;
         }
         // get offset in hours from "real noon" and local noon.
         public static double GetOffset(double day) {
             double angle = (2 * Math.PI * day)/yearLength;
             // an approximation of the equation of time.
-            return 7.5 * Math.Sin(angle - 3.3) +
+            double offset = 7.5 * Math.Sin(angle - 3.3) +
                 9.8 * Math.Sin(2 * angle - 2.9);
+            // offset in hours
+            return offset / 60.0;
         }
         static void Usage() {
             Console.WriteLine("usage: " + programName + " [-v | --version] [-h | --help]");
+        }
+        static void Version() {
+            Console.WriteLine(programName + " " + version);
         }
         static void Main(String[] args) {
             for(int i = 0; i < args.Length; i++) {
                 switch(args[i]) {
                     case "-v":
                     case "--version":
+                        Version();
                         Environment.Exit(0);
                         break;
                     case "-h":
                     case "--help":
+                        Usage();
                         Environment.Exit(0);
                         break;
                 }
@@ -59,34 +72,50 @@ namespace BesselCli {
                   || month > 12 || month < 1) {
                 Console.WriteLine(invalid);
             }
-            byte monthLength;
-            // decide the month length
-            switch(month) {
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    monthLength = 31;
-                    break;
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    monthLength = 30;
-                    break;
-                case 2:
-                    // assuming a Julian calendar for simplicity
-                    monthLength = (byte) (((year % 4) == 0) ? 28 : 29);
-                    break;
-            }
             byte day;
             Console.WriteLine("Day: ");
-            while(!Byte.TryParse(Console.ReadLine(), out day) || day > 31) {
+            while(!Byte.TryParse(Console.ReadLine(), out day)
+                  || day > 31 || day < 1) {
                 Console.WriteLine(invalid);
             }
+            byte monthLength = 0;
+            // turn days of the month into days of the year
+            for(int i = 0; i < month-1; i++) {
+                switch(i+1) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        monthLength = 31;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        monthLength = 30;
+                        break;
+                    case 2:
+                        // assuming a Julian calendar for simplicity
+                        monthLength = (byte) (((year % 4) == 0) ? 28 : 29);
+                        break;
+                    default:
+                        Console.WriteLine("Stop.");
+                        Environment.Exit(0);
+                        break;
+                }
+                day += monthLength;
+            }
+            double declension = GetDeclension(day);
+            double offset = GetOffset(day);
+            double hours = (12.0/Math.PI) * GetHours((Math.PI / 180.0) * latitude,
+                                                     declension * (Math.PI / 180.0));
+            double noon = 12 + offset;
+            double sunrise = noon - hours;
+            double sunset = noon + hours;
+            Console.WriteLine("Results:\nSunrise: " + sunrise + "\nSunset: " + sunset);
         }
     }
 }
